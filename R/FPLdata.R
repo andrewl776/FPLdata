d6fc8d2e3279a046f597743baa3069d646543125
Andrew Little <alittle@ascent.io>
2021-12-02 14:07:10 +0000

    CRAN resubmit

--- R/FPLdata.R
+++ R/FPLdata.R
@@ -6,7 +6,7 @@
 #' This data contains a large variety of information on players and their
 #' current attributes on
 #' \href{https://fantasy.premierleague.com/}{Fantasy Premier League}. In particular, it contains a
-#' `next_gw_points` (next gameweek points) value for each player given their
+#' next_gw_points (next gameweek points) value for each player given their
 #' attributes in the current week. Rows represent player-gameweeks, i.e. for
 #' each player there is a row for each gameweek. This makes the data suitable
 #' for modelling a player's next gameweek points, given attributes such as
